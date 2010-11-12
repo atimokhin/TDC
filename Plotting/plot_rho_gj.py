6ca380258d5fb5b74481e43c841fa6ec2cde87fa
Andrey Timokhin <atimokhin@gmail.com>
2010-11-11 23:14:02 -0800

    Plotting/plotting.mk adjusted,  make.depend deleted from repository
    	modified:   .gitignore              exclude make.depend
    	modified:   @tests/tests.mk
    	modified:   Plotting/plot_rho_gj.py
    	modified:   Plotting/plotting.mk    not it participates in depend
    	deleted:    make.depend
    	modified:   makefile

--- Plotting/plot_rho_gj.py
+++ Plotting/plot_rho_gj.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+
+# PDG Boiler plate ==========================================
 # append module search path for ATbase
 import sys
 sys.path.append('/home/atim/WORK/C++/ATbase2/lib_python/')
@@ -5,8 +10,22 @@ sys.path.append('/home/atim/WORK/C++/ATbase2/lib_python/')
 import _py_PDG__rho_gj
 # import ATbase module
 import ATbase as AT
+# ============================================================
+
+
+filename='cascade.input'
 
 #initialize PlotData1D object
-pd=AT.PlotData1D(_py_PDG__rho_gj.PDG__rho_gj('../cascade.input',100))
+pd=AT.PlotData1D(_py_PDG__rho_gj.PDG__rho_gj(filename,100))
 
-# do whatever your want :) ..
+# do whatever your want :)
+
+ax = plt.subplot(111)
+ax.plot(pd.x,pd.y)
+ax.set_xlim(pd.xlim)
+ax.set_ylim([-2.1,2.1])
+
+ax.set_xlabel('$x$')
+ax.set_ylabel(r'$\rho_\mathrm{GJ}$')
+ax.set_title(r'$\rho_\mathrm{GJ}$' +' from \"%s\"' % filename)
+plt.show()
