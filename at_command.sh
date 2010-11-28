d381b17d3bd5bd69bdfe45d6e843fa017808f5c8
Andrey Timokhin <atimokhin@gmail.com>
2010-11-28 13:58:30 -0800

    small fix

--- at_command.sh
+++ at_command.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #PBS -N SCLF_jp0.5_Ap0.3
-#PBS -e SCLF_jp0.5_Ap0.3.err
-#PBS -o SCLF_jp0.5_Ap0.3.out
+#PBS -e ../RESULTS/SCLF_jp0.5_Ap0.3.err
+#PBS -o ../RESULTS/SCLF_jp0.5_Ap0.3.out
 
 #PBS -l nodes=1:ppn=1,walltime=72:00:00
 #PBS -q queuename
