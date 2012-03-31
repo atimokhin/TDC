a81b954b6d0ab6d212f86df5f2d7b9e86d2d5b5a
Andrey Timokhin <atimokhin@gmail.com>
2012-03-31 14:58:33 -0700

    minor changes in auxiliary files
    	modified:   at_command.sh
    	new file:   at_command__prop.sh
    	modified:   cascade.input

--- at_command__prop.sh
+++ at_command__prop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#PBS -N Arons__j1.059_inj12
-#PBS -e ../RESULTS/Arons__j1.059_inj12.err
-#PBS -o ../RESULTS/Arons__j1.059_inj12.out
+#PBS -N Prop_jm1.5
+#PBS -e ../RESULTS/Prop_jm1.5.err
+#PBS -o ../RESULTS/Prop_jm1.5.out
 
 #PBS -l nodes=1:ppn=1,walltime=72:00:00
 #PBS -q henyey_serial 
@@ -15,4 +15,4 @@ then
 fi
 
 # start simulations
-./run_cascade.exe
+./prop.exe --noPairs --id=SCLF__jm1.5_Pcf1e8_L1_nGJ5e4_nx5e3_dt4e-5__RhoGJConst__R6C_Xb0.7__dP5e-2_inj8_s1
